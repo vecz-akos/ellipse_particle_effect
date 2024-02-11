ecb34afdb3aabddeb4c1be3ff445bae975c84733
Akos Oravecz <oraveczakos2001@gmail.com>
2024-02-11 10:21:15 +0100

    small improvements to concept

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const canvas = document.querySelector('canvas'),
 
 let width = canvas.width = window.innerWidth,
     height = canvas.height = window.innerHeight,
-    frame = 0
+    frame = 0,
+    animate = true,
+    clearCanvas = true;
 
 class Particle{
     constructor(x=0, y=0) {
@@ -12,15 +14,18 @@ class Particle{
     }
 
     update(circle) {
-        const d = this.distance(circle)/3
+        //const d = Math.min(this.distance(circle)/Math.log10(this.distance(circle)+1) + this.distance({x:0, y:0, r:300})/Math.log10(this.distance(circle)+1))
+        //const d = Math.min(this.distance(circle)/8, this.distance({x:100, y:100, r:350})/8)
+        //const d = this.distance(circle)/8
+        const x = this.distance(circle)
+        const d = Math.sqrt(x+8)*3-7
         const angle = Math.random()*Math.PI*2
-        debugger
         this.x += d * Math.cos(angle)
         this.y += d * Math.sin(angle)
     }
 
-    distance(obj=new Particle()) {
-        return Math.sqrt(this.dummyDistance(obj))
+    distance(obj) {
+        return Math.abs(Math.sqrt(this.dummyDistance(obj)) - obj.r)
     }
 
     dummyDistance(obj=new Particle()) {
@@ -33,7 +38,7 @@ class Particle{
 let circle = {
     x: Math.round(width/2),
     y: Math.round(height/2),
-    r: Math.round(Math.min(width, height)*(2/3))
+    r: Math.round(Math.min(width, height)*(1/3))
 }
 
 let particles = []
@@ -49,8 +54,10 @@ const init = () => {
 }
 
 const update = () => {
-    for (let p of particles) {
-        p.update(circle)
+    if (animate) {
+        for (let p of particles) {
+            p.update(circle)
+        }
     }
 
     draw()
@@ -58,10 +65,12 @@ const update = () => {
 }
 
 const draw = () => {
-    ctx.fillStyle = "#fff"
-    ctx.fillRect(0, 0, width, height)
+    if (clearCanvas) {
+        ctx.fillStyle = "#fff"
+        ctx.fillRect(0, 0, width, height)
+    }
 
-    ctx.fillStyle = "#00000044"
+    ctx.fillStyle = "#00000088"
 
     particles.forEach((p) => {
         ctx.beginPath()
@@ -72,6 +81,20 @@ const draw = () => {
 
 window.onload = () => {
     init()
+
+    document.body.addEventListener("keydown", (event) => {
+        switch (event.key) {
+            case " ":
+                animate = !animate
+                break
+            case "c":
+                clearCanvas = !clearCanvas
+                break
+            default:
+                break
+        }
+    })
+
     window.requestAnimationFrame(update)
 }
 
